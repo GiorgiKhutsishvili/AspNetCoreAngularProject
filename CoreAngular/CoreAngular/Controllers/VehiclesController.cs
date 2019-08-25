2e80c7e062445e761d014733bfbd18db050607b4
GiorgiKhutsishvili <34691385+GiorgiKhutsishvili@users.noreply.github.com>
2019-08-25 11:26:31 -0700

    Commit new

--- CoreAngular/CoreAngular/Controllers/VehiclesController.cs
+++ CoreAngular/CoreAngular/Controllers/VehiclesController.cs
@@ -8,6 +8,7 @@ using CoreAngular.Models;
 using CoreAngular.Resources;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoreAngular.Controllers
 {
@@ -25,18 +26,78 @@ namespace CoreAngular.Controllers
             this.context = context;
         }
         [HttpPost]
-        public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
+        public async Task<IActionResult> CreateVehicle([FromBody] SaveVehicleResource vehicleResource)
         {
-            var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var vehicle = mapper.Map<SaveVehicleResource, Vehicle>(vehicleResource);
 
             vehicle.LastUpdate = DateTime.Now;
 
             context.Vehicles.Add(vehicle);
             await context.SaveChangesAsync();
 
-            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            var result = mapper.Map<Vehicle, SaveVehicleResource>(vehicle);
 
             return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVehicle(int id, [FromBody] SaveVehicleResource vehicleResource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+            if (vehicle == null)
+                return NotFound();
+
+            mapper.Map<SaveVehicleResource, Vehicle>(vehicleResource, vehicle);
+
+            vehicle.LastUpdate = DateTime.Now;
+
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, SaveVehicleResource>(vehicle);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle = await context.Vehicles.FindAsync(id);
+
+            if (vehicle == null)
+                return NotFound();
+
+            context.Remove(vehicle);
+            await context.SaveChangesAsync();
+
+            return Ok(id);
+
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id)
+        {
+            var vehicle = await context.Vehicles
+                .Include(v => v.Features)
+                .ThenInclude(vf => vf.Feature)
+                .Include(v => v.Model)
+                .ThenInclude(m => m.Make)
+                .SingleOrDefaultAsync(v => v.Id == id);
+
+            if (vehicle == null)
+                return NotFound();
+
+            var vehicleResource = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(vehicleResource);
+
         }
     }
 }
--- CoreAngular/CoreAngular/Persistance/VegaDbContext.cs
+++ CoreAngular/CoreAngular/Persistance/VegaDbContext.cs
@@ -11,7 +11,7 @@ namespace AspNetCoreAngular.Persistance
     {
         public DbSet<Vehicle> Vehicles { get; set; }
         public DbSet<Make> Makes { get; set; }
-
+        public DbSet<Model> Models { get; set; }
         public DbSet<Feature> Features { get; set; }
 
         public VegaDbContext(DbContextOptions<VegaDbContext> options)
